1473c95ef2f630ea02d1c1fc50954295ea835112
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-24 00:41:23 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240124/삼항연산자/ternary-operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int s;
+    char d[100];
+    scanf("%d",&s);
+
+    d = s=100 : "pass" : "failure";
+    printf("%s",d);
+    return 0;
+}
