ad519e3369fd7056fb5ffb2b21b3fbb96f3058a1
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-24 15:41:20 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240124/두 번의 연산/two-operations.c
+++ 240124/두 번의 연산/two-operations.c
@@ -8,8 +8,11 @@ int main() {
     if(a%2==1){
         a = a+3;
     }
-   
+    else if(a % 3==0){
         printf("%d",a/3);
-    
+    }
+    else   
+        printf("%d",a);
+        
     return 0;
 }
