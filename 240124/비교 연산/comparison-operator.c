865b0f73f064d64be71dde898f250ed65452700a
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-24 16:00:00 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/비교 연산/comparison-operator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if(a>=b){
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+
+    if(a>b){
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+
+    if(a<=b){
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+
+    if(a==b){
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+
+    if(a=!b){
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+    return 0;
+}
