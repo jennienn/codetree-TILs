97c11ad832786712eff65123fdab7d2f5549228e
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-24 15:44:25 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240124/두 번의 연산/two-operations.c
+++ 240124/두 번의 연산/two-operations.c
@@ -3,17 +3,16 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a;
-    scanf("%d",&a);
+    scanf("%d", &a);
 
-    if(a%2==1){
-        a = a+3;
+    if (a % 2 == 1) {
+        a = a + 3;
+    } else if (a % 3 == 0) {
+        printf("%d", a / 3);
+        return 0;  // 프로그램 종료
     }
-    else if(a % 3==0){
-        printf("%d",a/3);
-        return 0;
-    }
-      
-    printf("%d",a);
+
+    printf("%d", a);
 
     return 0;
 }
