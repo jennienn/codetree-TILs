f62fdcc66bfb5b07814180224f9d2032945781ce
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-24 15:51:34 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/두 번의 연산/two-operations.c
+++ 240124/두 번의 연산/two-operations.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    // 정수 a를 입력받음
-    int a;
-    scanf("%d", &a);
 
-    // a가 홀수인 경우 3을 더함
-    if (a % 2 == 1) {
-        a += 3;
-        printf("%d", a);
-    }
+int a;
+scanf("%d", &a);
 
-    // 최종적으로 a가 3의 배수인 경우 3으로 나눔
-    if (a % 3 == 0) {
-        a /= 3;
-        printf("%d", a);
-    } else {
-        printf("%d", a);
-    }
+if (a % 2 == 1) {
+    a = a + 3;
+} 
+if (a % 3 == 0) {
+    printf("%d", a / 3);
+    return 0;  
+}
 
-    return 0;
+printf("%d", a);
+
+return 0;
 }
