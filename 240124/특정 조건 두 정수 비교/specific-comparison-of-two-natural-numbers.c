c280cdaee7291703a57696920505f66fab16064f
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-24 15:56:02 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
+++ 240124/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -6,16 +6,16 @@ int main() {
     scanf("%d %d",&a,&b);
 
     if(a<b){
-        printf("1");
+        printf("1 ");
     }
     else{
-        printf("0");
+        printf("0 ");
     }
     if(a==b){
-        printf("1");
+        printf("1 ");
     }
     else{
-        printf("0");
+        printf("0 ");
     }
     return 0;
 }
