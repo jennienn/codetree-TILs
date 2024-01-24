6756222622c10caa10b99151c425f8d13e31a2f2
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-24 15:33:56 +0900

    Title: 최대 2번의 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/최대 2번의 연산/up-to-2-calculations.c
+++ 240124/최대 2번의 연산/up-to-2-calculations.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 변수 선언 및 입력
     int a;
-    scanf("%d",&a);
-
-    if(a % 2 == 0){
-        printf("%d",a/2);
-    }
-    else if(a % 2 == 1){
-        printf("%d",(a+1)/2);
-    }
+    scanf("%d", &a);
+    
+    if(a % 2 == 0)
+        a /= 2;
+    
+    if(a % 2 == 1)
+        a = (a + 1) / 2;
+    
+    printf("%d", a);
     return 0;
 }
