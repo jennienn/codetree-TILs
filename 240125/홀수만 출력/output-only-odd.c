804d4aead1679029986dd8c9c7ca7182df9dada9
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-25 22:11:29 +0900

    Title: 홀수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/홀수만 출력/output-only-odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+
+    scanf("%d %d",&a,&b);
+
+    for(int i=a; i<=b; i += 2){
+        printf("%d ",i);
+    }
+    return 0;
+}
