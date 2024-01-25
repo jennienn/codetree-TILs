6f8043a8ad6fbc665b7cb1ead000a9223ba582a6
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-25 21:20:29 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/코로나 메뉴얼/covid-manual.c
+++ 240125/코로나 메뉴얼/covid-manual.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-
-    char a,b,c;
-    int a1,b1,c1;
-    int s=0;
-
-    scanf("%c %d\n%c %d\n%c %d\n",&a,&a1,&b,&b1,&c,&c1);
-
-    if(a=='Y' && a1>=37){
-        if((b=='Y' && b1>=37) || (c=='Y'&&c1>=37)){
+   
+    char c1, c2, c3;
+    int t1, t2, t3;
+    
+    // 입력
+    scanf(" %c %d", &c1, &t1);
+    scanf(" %c %d", &c2, &t2);
+    scanf(" %c %d", &c3, &t3);
+    
+    // A가 2명 이상인지 판단하기
+    if(c1 == 'Y' && t1 >= 37) {
+        // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됨
+        if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
             printf("E");
-        }
-        else{
+        else
             printf("N");
-        }
-    else{
-        if((b=='Y' && b1>=37) && (c=='Y'&&c1>=37)){
-            printf("E");
-        }
-        else{
-            printf("N");
-        }
     }
+    else {
+        // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 함
+        if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
+            printf("E");
+        else
+            printf("N");
     }
     return 0;
 }
