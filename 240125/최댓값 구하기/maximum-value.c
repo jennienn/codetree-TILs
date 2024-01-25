7c4add56889272d088cccd52bb75fdaa3f237a5b
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-25 21:07:00 +0900

    Title: 최댓값 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240125/최댓값 구하기/maximum-value.c
+++ 240125/최댓값 구하기/maximum-value.c
@@ -5,14 +5,19 @@ int main() {
 
     scanf("%d %d %d",&a,&b,&c);
 
-    if(a>=b && a>=c){
-        printf("%d",a);
+    if(a>=b){
+        if(a>=c){
+            printf("%d",&a);
+        }
+        else{
+            printf("%d",&c);
+        }
+    
+    else{
+        if(b>=c){
+            printf("%d",&b);
+        }
     }
-    if(b>=c && b>=a){
-        printf("%d",b);
-    }
-    if(c>=a && c>=b){
-        printf("%d",c);
     }
     
     return 0;
