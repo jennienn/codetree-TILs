af6470121b8e6f60a6f0f2a4d19dd374001d809c
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-25 23:51:20 +0900

    Title: 자연수면 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240125/자연수면 출력/output-only-natural-number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if(a>0){
+        for(int i=0; i<n; i++){
+            printf("%d",i);
+        }
+    }
+    else{
+        printf("0");
+    }
+    return 0;
+}
