b8e3a03b300119984213ca782fc35e253438f88d
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-25 23:54:52 +0900

    Title: 2 또는 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/2 또는 3의 배수/multiple-of-2-or-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a;
+
+    scanf("%d", &a);
+
+
+    for(int i=1; i<=a; i++){
+        if(i%2==0 || i%3==0)
+            printf("1 ");
+        else{
+            printf("0 ");
+        }
+    }
+
+
+
+    return 0;
+}
