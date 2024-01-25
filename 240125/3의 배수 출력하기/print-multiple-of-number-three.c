4f7b1c3fa2b459198d39864dbf97670aa83c834a
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-25 23:02:42 +0900

    Title: 3의 배수 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/3의 배수 출력하기/print-multiple-of-number-three.c
+++ 240125/3의 배수 출력하기/print-multiple-of-number-three.c
@@ -2,13 +2,13 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int i=1;
+    int i=3;
     int n;
     scanf("%d",&n);
 
-    while(i>=n){
-        printf("%d ",n);
-        n *= 3;
+    while(i <= n){
+        printf("%d ",i);
+        i += 3;
     }
     return 0;
 }
