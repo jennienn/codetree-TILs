786f7022eb1968e2e8615c88f1f5da7b60fcc656
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-26 19:44:36 +0900

    Title: a 부터 b 까지, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240126/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if(a%2==0){
+        for(int i=a; i>=b; i*=2){
+            printf("%d ",i);
+        }
+    }
+    else{
+        for(int i=a; i>=b; i*=3){
+            printf("%d ",i);
+        }
+    }
+
+    return 0;
+}
