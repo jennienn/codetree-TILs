8c6482c7b868cfc2ad3327815fb86fb8e16bafad
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-26 21:56:35 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int num[10];
+    int cnt=0;
+
+    for(int i=0; i<10; i++){
+        scanf("%d\n",&num[i]);
+    }
+
+    for(int i=0; i<10; i++){
+        if(num[i]%2==1){
+            cnt++;
+        }
+    }
+
+    printf("%d",cnt);
+
+    return 0;
+}
