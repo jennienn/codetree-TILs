2edf2123d57280cafe3a31300d2b220a2932e140
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-27 15:15:43 +0900

    Title: 등장횟수 확인, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240127/등장횟수 확인/check-number-of-appearances.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num[5];
+    int cnt=0;
+
+    for(int i=0; i<5; i++){
+        scanf("%d\n",num[i]);
+    }
+
+    for(int i=0; i<5; i++){
+        if(num[i]%2==0)
+            cnt++;
+    }
+
+    printf("%d",cnt);
+
+
+
+    return 0;
+}
