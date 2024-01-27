703fea2415570e54320f61771e2f19c4c268f638
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-27 15:51:59 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/범위 안의 수 2/number-in-range-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int num[10];
+    for(int i=0; i<10; i++){
+        scanf("%d\n",&num[i]);
+    }
+
+    int sum=0;
+    int cnt=0;
+
+    for(int i=0; i<10; i++){
+        if(i>=0 && i<=200){
+            cnt++;
+            sum += i;
+        }
+    }
+
+    printf("%d %.1f", sum, (float)sum/cnt);
+    return 0;
+}
