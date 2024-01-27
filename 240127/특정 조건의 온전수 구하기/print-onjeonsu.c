5202221f301cfa8bd3074713dfb11c6c54492105
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-27 17:37:43 +0900

    Title: 특정 조건의 온전수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/특정 조건의 온전수 구하기/print-onjeonsu.c
+++ 240127/특정 조건의 온전수 구하기/print-onjeonsu.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(int i=0; i<n; i++){
-        if(i%2==0)
-            continue;
-        if(i%10==5)
-            continue;
-        if(i%3==0 && i%9!=0)
-            continue;
-        printf("%d ",i);
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0 && i % 10 != 5 && (i % 3 != 0 || i % 9 == 0)) {
+            printf("%d ", i);
+        }
     }
+
     return 0;
 }
