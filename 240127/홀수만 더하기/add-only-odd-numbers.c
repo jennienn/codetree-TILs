4d77ae2ee7293b36bb5a9cbc8c929f93dfb7413e
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-27 15:31:14 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/홀수만 더하기/add-only-odd-numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int num[n];
+
+    for(int i=0; i<n; i++){
+        scanf("%d\n",&num[i]);
+    }
+
+    int sum;
+
+    for(int i=0; i<n; i++){
+        if(i%2==1 && i%3==0){
+            sum += i;
+        }
+    }
+
+    printf("%d",sum);
+    return 0;
+}
