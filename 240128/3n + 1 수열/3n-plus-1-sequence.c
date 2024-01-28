d89885d7a4f0f8ec7e42d38d4ad261dc725aa213
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-28 17:14:45 +0900

    Title: 3n + 1 수열, Time: 1014ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240128/3n + 1 수열/3n-plus-1-sequence.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt=0;
+    scanf("%d",&n);
+
+    while(1){
+        if(n!=1){
+
+            if(n%2==0){
+                n /= 2;
+                cnt++;
+            }
+
+            else if(n%2==1){
+                n = n*3+1;
+                cnt++;
+            }
+
+        }
+    }
+
+    printf("%d",cnt);
+    return 0;
+}
