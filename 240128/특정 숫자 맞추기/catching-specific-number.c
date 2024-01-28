7f52bfacf23240afde429db739919583f30f0692
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-28 15:39:13 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/특정 숫자 맞추기/catching-specific-number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    while(1){
+        int n;
+        scanf("%d", &n);
+
+        if(n<25)
+            printf("Higher\n");
+
+        else if(n>25)
+            printf("Lower\n");
+            
+        else if(n==25){
+            printf("Good\n");
+            break;
+        }
+    }
+    return 0;
+}
