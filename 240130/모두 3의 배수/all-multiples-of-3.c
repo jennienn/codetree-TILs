1e421e18fb3507a630b9ccaef4f8b661ae7ecfde
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-30 00:10:53 +0900

    Title: 모두 3의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240130/모두 3의 배수/all-multiples-of-3.c
+++ 240130/모두 3의 배수/all-multiples-of-3.c
@@ -5,19 +5,19 @@ int main() {
     // 여기에 코드를 작성해주세요.
     
     int n;
-    int cnt=0;
     
     for(int i=0; i<5; i++){
         scanf("%d\n",&n);
     }
+    bool s = true;
 
     for(int i=0; i<5; i++){
-        if(n%3==0){
-            cnt++;
+        if(n%3 !=0){
+            s = false;
         }
     }
 
-    if(cnt==5){
+    if(s==true){
         printf("1");
     }
     else{
