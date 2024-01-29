85971d0bbea37d468a32f7568ddbc6ec8e709aed
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-29 14:37:44 +0900

    Title: 합성수 판별기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240129/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+
+    bool s = false;
+    
+    for(int i=2; i<n; i++){
+        if(n % i ==0)
+            s=ture;
+    }
+
+    if(s==true){
+        printf("C");
+    }
+    else
+        printf("N");
+        
+    return 0;
+}
