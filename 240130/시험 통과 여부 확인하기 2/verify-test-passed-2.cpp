cfe3ceb270998561d88e1be203a21911161fd6c4
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-30 18:34:22 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int cnt=0;
+
+    for(int i=0; i<n; i++){
+
+        int sum=0;
+        int a[4];
+
+        for(int j=0; j<4; j++){
+            cin >> a[j];
+        }
+
+        for(int j=0; j<4; j++){
+            sum += a[j];
+        }
+
+        double avg = (double)sum/4;
+
+
+        if(avg>=60){
+            cout << "pass" << endl;
+            cnt++;
+        }
+        else{
+            cout << "fail" << endl;
+        }
+    }
+
+    cout << cnt;
+
+
+
+
+    return 0;
+}
