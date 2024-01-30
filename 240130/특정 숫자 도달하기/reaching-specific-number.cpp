ccbc50396ec9a2b9684ce918901f55ca940110a2
김예진 <118978246+jennienn@users.noreply.github.com>
2024-01-30 17:44:30 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240130/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a[10];
+    int cnt=0;
+    int sum=0;
+
+    for(int i=0; i<10; i++){
+        cin >> a[i];
+    }
+
+    for(int i=0; i<10; i++){
+        if(a[i]>=250){
+            break;
+            sum += a[i];
+            cnt++;
+        }
+    }
+
+    double avg = (double)sum/ cnt;
+
+    cout << fixed;
+    cout.precision(1)
+    cout << sum << " " << avg;
+
+    return 0;
+}
