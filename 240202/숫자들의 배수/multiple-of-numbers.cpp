344a8c2c05f8e30c26a75d5d32b4004fe924176e
김예진 <118978246+jennienn@users.noreply.github.com>
2024-02-02 20:52:37 +0900

    Title: 숫자들의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240202/숫자들의 배수/multiple-of-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a[10];
+    int cnt;
+
+    cin >> a[0];
+
+    for(int i=1; i<10; i++){
+        a[i] = a[i-1]+a[0];
+    }
+
+    for(int i=0; i<10; i++){
+        cout << arr[i] << " ";
+        if(arr[i]%5==0)
+            cnt++;
+        if(cnt >=)
+            break;
+    }
+    
+    return 0;
+}
