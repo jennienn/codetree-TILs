6524cbe7815aa3c8d5d78fb8fc173fcf3a42b613
김예진 <118978246+jennienn@users.noreply.github.com>
2024-02-05 16:27:08 +0900

    Title: 개수 세기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/개수 세기/count-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n,m;
+    cin >> n >> m;
+    int a[n];
+    int cnt=0;
+
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    for(int i=0; i<n; i++){
+        if(a[i]==m){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
