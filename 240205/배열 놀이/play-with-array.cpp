7ca0df51f1602dea041664606c8bad627444a489
김예진 <118978246+jennienn@users.noreply.github.com>
2024-02-05 17:10:49 +0900

    Title: 배열 놀이, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240205/배열 놀이/play-with-array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    int a[n + 1];
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i < q; i++) {
+        int Q, N, N2;
+        cin >> Q;
+
+        if (Q == 1 || Q == 2) {
+            cin >> N;
+        } else {
+            cin >> N >> N2;
+        }
+
+        if (Q == 1) {
+            cout << a[N] << endl;
+        } else if (Q == 2) {
+            int foundindex = 0;
+            for (int j = 1; j <= n; j++) {
+                if (a[j] == N) {
+                    foundindex = j;
+                    break;
+                }
+            }
+            cout << foundindex << endl;
+        } else if (Q == 3) {
+            for (int j = N; j <= N2; j++) {
+                cout << a[j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
