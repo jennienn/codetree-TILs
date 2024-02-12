e5a8ba9d14b20d11b42e15852e4331c7b912006f
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-02-12 18:17:15 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+
+    cin >> a >> b;
+
+    for(int i = 1; i<10; i++){
+        for(int j=b; j>=a; j--){
+            if(j%2==0){
+                cout << j << " * " << i << " = " << i*j;
+                    if(j!=a){
+                        cout << " / ";
+                    }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
