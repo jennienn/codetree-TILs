b21f04b9ddec1d47577e45b5c8c0770d54ce0eb8
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-02-15 18:57:07 +0900

    Title: ab곱 n번 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240215/ab곱 n번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int a,b;
+        cin >> a >> b;
+        int sum = 1;
+        for(int j=a; j<=b; j++){
+           sum *= j;
+        }
+        cout << sum << endl;;
+    }
+    return 0;
+}
