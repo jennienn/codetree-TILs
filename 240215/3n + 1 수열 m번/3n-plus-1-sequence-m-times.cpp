8132b6dd5bda86a273e320004ee95663c18487a5
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-02-15 19:04:12 +0900

    Title: 3n + 1 수열 m번, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240215/3n + 1 수열 m번/3n-plus-1-sequence-m-times.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m;
+    cin >> m;
+
+    for(int i=0; i<m; i++){
+        int n;
+        cin >> n;
+        int cnt=0;
+
+        while(n!=1){
+            if(n%2==0){
+                n = n / 2;
+                cnt++;
+            }
+            else{
+                n = (n*3)+1;
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
