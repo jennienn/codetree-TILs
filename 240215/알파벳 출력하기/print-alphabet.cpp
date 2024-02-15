b59f4a80a33d5121adea6145f046763df9672bd4
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-02-15 18:41:33 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240215/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    char cnt = 'A';
+
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<i; j++){
+            cout << cnt;
+            cnt++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
