6ee001f7d45a0d61e258bd98784fc4c33e91632f
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-02-18 17:04:43 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240218/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
+++ 240218/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
+
+#define MAX_NUM 1000
+
 using namespace std;
-#define MAX 1000
 
 int main() {
-    
-    int n;
-    int price[MAX];
 
+    int n;
+    int price[MAX_NUM];
+
+    // 입력:
     cin >> n;
-    for(int i=0; i<n; i++){
+    for(int i = 0; i < n; i++)
         cin >> price[i];
-    }
 
     // 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택
     int max_profit = 0;
-    for(int i=0; i<n; i++){
+    for(int i = 0; i < n; i++) {
         // 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택
-        for(int j=i+1; j<n; j++){
-            int profit = price[j] - price[i];
-
-            if(profit > max_profit){
+        for(int j = i + 1; j < n; j++) {
+			int profit = price[j] - price[i];
+            
+            if(profit > max_profit)
                 max_profit = profit;
-            }
         }
-
-        cout << max_profit;
     }
+
+    cout << max_profit;
     return 0;
 }
