3817011677a08026fe474bd43ba66a353645a720
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-02-18 17:59:48 +0900

    Title: 대문자로 바꾸기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240218/대문자로 바꾸기/change-to-capital.cpp
+++ 240218/대문자로 바꾸기/change-to-capital.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    char arr[5][3];
-
-    for(int i=0; i<5; i++){
-        for(int j=0; j<3; j++){
-            cin >> arr[i][j];
-        }
-    }
-
-    char cnt = 'A';
-
-    for(int i=0; i<5; i++){
-        for(int j=0; j<3; j++){
-            cout << cnt << " ";
-            cnt++;
-        }
-        cout << endl;
-    }
-
+ 
+	char arr_2d[5][3];
+	
+	for(int i = 0; i < 5; i++)
+		for(int j = 0; j < 3; j++)
+			cin >> arr[i][j];
+	
+	for(int i = 0; i < 5; i++) {
+		for(int j = 0; j < 3; j++) {
+			arr[i][j] += 'A' - 'a'; // 대문자로 변경
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
 
     return 0;
 }
