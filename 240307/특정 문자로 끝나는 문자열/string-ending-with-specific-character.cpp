1d7a1295a349df57716037f327f461413fe006ff
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-03-07 20:24:08 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240307/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string str[10];
+
+    for(int i=0; i<10; i++){
+        cin >> str[i];
+    }
+
+    char s;
+    cin >> s;
+    int cnt = 0;
+
+    for(int i=0; i<10; i++){
+       int len = str[i].length();
+        if(str[i][len-1]==s){
+            cout << str[i] << endl;
+            cnt++;
+       }
+
+       if(cnt == 0){
+        cout << "None";
+       }
+    }
+    return 0;
+}
