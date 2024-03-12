4953572c02d22e176303b6c1c6ca51c7f397ca59
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-03-12 17:01:05 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    string str[n];
+    for(int i=0; i<n; i++){
+        cin >> str[i];
+    }
+
+    char s;
+    cin >> s;
+
+    int cnt=0;
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        if(str[i][0]==s){
+            cnt++;
+            int len = str[i].length();
+            sum += len;
+        }
+    }
+
+    float avg = (float)sum/cnt;
+    
+    cout << fixed;
+    cout.precision(2);
+    cout << cnt << " " << avg;
+
+    return 0;
+}
