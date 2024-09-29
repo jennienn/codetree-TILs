041689cd47acc04bd29cf42039247b99ced3c5eb
YEJIN KIM 👩🏻‍💻 <118978246+jennienn@users.noreply.github.com>
2024-09-30 00:30:43 +0900

    Title: 최대공약수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240930/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 240930/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void FindGcd(int n, int m){
 
     int gcd = 0;
-    for(int i=1; i<min(n,m); i++){
+    for(int i=1; i<=min(n,m); i++){
         if(n%i==0 && m%i==0){
             gcd = i;
         }
