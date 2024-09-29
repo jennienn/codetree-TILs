1c5e9b5ff293d7a02023455a239e890e28246b7c
YEJIN KIM 👩🏻‍💻 <118978246+jennienn@users.noreply.github.com>
2024-09-30 00:29:53 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void FindGcd(int n, int m){
+
+    int gcd = 0;
+    for(int i=1; i<min(n,m); i++){
+        if(n%i==0 && m%i==0){
+            gcd = i;
+        }
+    }
+    cout << gcd;
+}
+int main() {
+    
+    int n,m;
+    cin >> n >> m;
+
+    FindGcd(n,m);
+    
+    return 0;
+}
