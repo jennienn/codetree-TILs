93792469ff723f85ef7ca99cf910d331b4e2e995
YEJIN KIM 👩🏻‍💻 <118978246+jennienn@users.noreply.github.com>
2024-10-16 16:18:57 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241016/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void cal(int a, char o, int c){
+    if(o == '+'){
+        cout << a << " + " << c << " = " << a+c << endl;
+    }else if(o == '-'){
+        cout << a << " - " << c << " = " << a-c << endl;
+    }else if(o=='/'){
+        cout << a << " / " << c << " = " << a/c << endl;
+    }else if(o=='*'){
+        cout << a << " * " << c << " = " << a*c << endl;
+    }else{
+        cout << "Fasle";
+    }
+}
+
+int main() {
+    
+    int a,c;
+    char o;
+
+    cin >> a >> o >> c;
+
+    cal(a,o,c);
+
+    return 0;
+}
